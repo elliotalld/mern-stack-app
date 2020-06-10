2b02e20d7533feeb2621f1f685b4cfa2fbd2e221
root <root@localhost.localdomain>
2020-06-10 16:59:06 -0500

    Updates

---
+++ precache-manifest.f1a849a12e3a31b8e7ecc63342440fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451eda7fb1c6f0a464fec665f40e190b",
+    "url": "/mern-stack-app%20/index.html"
+  },
+  {
+    "revision": "71f77d01cdf1be8529f8",
+    "url": "/mern-stack-app%20/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "d0e9e0438f8b36abc700",
+    "url": "/mern-stack-app%20/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "71f77d01cdf1be8529f8",
+    "url": "/mern-stack-app%20/static/js/2.bda0032f.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/mern-stack-app%20/static/js/2.bda0032f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e9e0438f8b36abc700",
+    "url": "/mern-stack-app%20/static/js/main.392b34b2.chunk.js"
+  },
+  {
+    "revision": "6c66c30cd9fcbf8e4d4d",
+    "url": "/mern-stack-app%20/static/js/runtime-main.1de320c4.js"
+  }
+]);
